6b1a12f3bc515b3b64a807cbe37ce0f29e741cce
sakainewman <c1032319@newcastle.ac.uk>
2025-05-14 12:26:34 +0100

    Implemented panel layout with panel library and tailwind

---
+++ frontend/src/components/terminalPanel.js
@@ -0,0 +1,7 @@
+export default function TerminalPanel() {
+    return (
+        <div className="h-full bg-[#48434B] p-2">
+            <p className="font-bold">Bottom Right Panel</p>
+        </div>
+    )
+}
